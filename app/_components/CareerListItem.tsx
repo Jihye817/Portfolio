5c5b27919512c371064d7c5f245293fec2fddb2b
jihye <wlgul817@gmail.com>
2025-05-15 15:21:38 +0900

    feat: 경력 기술 아이콘 삭제

--- app/_components/CareerListItem.tsx
+++ app/_components/CareerListItem.tsx
@@ -8,34 +8,28 @@ interface CareerProps {
 function CareerListItem({ career }: CareerProps) {
   return (
     <div className='mb-[3rem] flex gap-3'>
-      <div className='w-[4px] bg-gradient-to-t from-blue-300 to-blue-500'></div>
+      <div className='w-[4px] bg-gradient-to-t from-blue-300 to-blue-950'></div>
       <div className='flex flex-col'>
         <div className='mb-[0.5rem]'>
           <span className='mr-[1rem]'>
             {career.startDate} ~ {career.endDate}
           </span>
-          <span className='rounded-[0.5rem] bg-blue-500 px-[0.7rem] py-[0.2rem]'>
+          <span className='rounded-[0.5rem] bg-blue-500 px-[0.7rem] py-[0.2rem] text-white'>
             {career.position}
           </span>
         </div>
         <span className='text-[1.2rem] font-semibold'>{career.title}</span>
-        <span className='mb-[0.5rem] text-white/70'>{career.description}</span>
+        <span className='mb-[0.5rem] text-blue-950/70'>
+          {career.description}
+        </span>
         <span className='mb-[0.5rem] font-semibold'>기술</span>
         <div className='mb-[1rem] flex gap-[0.5rem]'>
           {career.skills.map(skill => (
             <div
-              className='group/skill relative rounded-[0.5rem] bg-white p-1'
+              className='group/skill relative flex gap-2 rounded-[0.5rem] bg-blue-500 p-1 px-[1rem]'
               key={skill}
             >
-              <Image
-                src={`/assets/icons/logo-${skill}.png`}
-                width={25}
-                height={25}
-                alt={skill}
-              ></Image>
-              <p className='invisible absolute bottom-[-2rem] left-[50%] z-100 translate-x-[-50%] rounded-[0.5rem] bg-blue-500 px-[0.7rem] py-[0.2rem] group-hover/skill:visible'>
-                {skill}
-              </p>
+              {skill}
             </div>
           ))}
         </div>
